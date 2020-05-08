8e55eb2700d67fc715ad9b1a9f7a3db4b989bfae
Na-Nazhou <nazhou.na9@gmail.com>
2020-05-08 19:24:08 +0800

    Rewrite squares instead of hardcoding

--- src/index.js
+++ src/index.js
@@ -19,24 +19,26 @@ class Board extends React.Component {
     />);
   }
 
+  renderRow(i) {
+    const items = []
+    for (let j = 0; j < 3; j++) {
+      items.push(this.renderSquare(i * 3 + j));
+    }
+    return (
+      <div className="board-row">
+        {items}
+      </div>
+    );
+  }
+
   render() {
+    const rows = [];
+    for (let i = 0; i < 3; i++) {
+      rows.push(this.renderRow(i));
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
