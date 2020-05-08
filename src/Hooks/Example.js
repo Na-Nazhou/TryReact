9493668bc95641ea3982fadfdaf5c8ce6c7af840
Na-Nazhou <nazhou.na9@gmail.com>
2020-05-09 03:55:56 +0800

    Add hook examples

---
+++ src/Hooks/Example.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+export const Example = () => {
+  // Declare a new state variable, which we'll call "count"  
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:  
+  // Runs after every render
+  // Schedule a different effect every time
+  // If your effect returns a function, React will run it when it is time to clean up
+  // Skip applying an effect if certain values haven't changed betweeen re-renders
+  // pass an array as an optional second argument to useEffect
+  // If [] is passed in, effect does not depend on any value from props / states
+  useEffect(() => {
+    // Update the document title using the browser API    
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </div>
+  );
+}
---
+++ src/Hooks/useReducer.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+export function useReducer(reducer, initialState) {
+  const [state, setState] = useState(initialState);
+
+  function dispatch(action) {
+    const nextState = reducer(state, action);
+    setState(nextState);
+  }
+
+  return [state, dispatch];
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+const initialState = { count: 0 };
+
+export const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+    </>
+  );
+}
